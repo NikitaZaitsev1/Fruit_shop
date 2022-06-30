447037a6c1f1053cd42e0ca8267529938657d513
Nikita Zaitsev <nikitazv90@gmail.com>
2022-06-30 12:24:09 +0300

    Update and delete methods model Post

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from post.views import PostListView, PostDetailView, tag_detail_view, post_new_view
+from post.views import PostListView, PostDetailView, tag_detail_view, post_new_view, post_edit_view, PostDeleteView
 
 urlpatterns = [
     path('post/', PostListView.as_view(), name='post_list_page'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail_page'),
     path('tag/<int:pk>/', tag_detail_view, name='tag_detail_page'),
     path('post/new/', post_new_view, name='post_new_page'),
+    path('post/edit/<int:pk>', post_edit_view, name='post_edit_page'),
+    path('post/<int:pk>/remove', PostDeleteView.as_view(), name='post_delete_page'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, DeleteView
 
 from post.forms import PostForm
 from post.models import Post, Tag
@@ -12,9 +13,8 @@ class PostListView(ListView):
     template_name = 'posts.html'
     context_object_name = 'posts'
     paginate_by = 3
+    ordering = ['-published_date']
 
-    def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
 class PostDetailView(DetailView):
@@ -42,3 +42,25 @@ def post_new_view(request):
     else:
         post_form = PostForm()
         return render(request, 'post_new.html', {'post_form': post_form})
+
+
+def post_edit_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            form.save_m2m()
+            return redirect('post_detail_page', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'post_form': form})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list_page')
